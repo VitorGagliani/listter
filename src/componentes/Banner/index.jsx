e31e358ad334c4363fe5e9f4ab609d41b14ca370
Vitor Gagliani <93485462+VitorGagliani@users.noreply.github.com>
2025-10-14 22:36:08 -0300

    animation

--- src/componentes/Banner/index.jsx
+++ src/componentes/Banner/index.jsx
@@ -1,11 +1,29 @@
 import './style.css'
+import BlurText from "./BlurText.js";
 
-export const Banner = () =>{
+
+export const Banner = ({handleAnimationComplete}) =>{
   return(
     <div className='banner'>
       <div className='banner-txt'>
-        <h1>Listter</h1>
-        <p>Seu facilitador de tarefas diárias</p>
+        <BlurText
+        text="Listter"
+        delay={100}
+        animateBy="words"
+        direction="top"
+        onAnimationComplete={handleAnimationComplete}
+        className="text-4xl mb-8"
+        
+/>
+        <BlurText
+        text="Seu facilitador de tarefas diárias"
+        delay={100}
+        animateBy="words"
+        direction="top"
+        onAnimationComplete={handleAnimationComplete}
+        className="text-2xl mb-8"
+/>
+       
       </div>
       <div className='banner-img'>
         <img src="/imagens/banner-img.svg"></img>
